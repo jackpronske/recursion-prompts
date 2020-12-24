1c21030fb7b619b6d276c099a2606a8f95922ca2
Jack <jackpronske@yahoo.com>
2020-12-24 13:05:28 -0800

    implemented a correct solution for countOccurance

--- src/recursion.js
+++ src/recursion.js
@@ -408,10 +408,23 @@ var fizzBuzz = function(n) {
 
 };
 
+
+
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  if (array[0] === value) {
+    return 1 + countOccurrence(array.slice(1), value);
+  }
+
+  return countOccurrence(array.slice(1), value);
+
 };
 
 // 21. Write a recursive version of map.
