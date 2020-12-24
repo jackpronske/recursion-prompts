fbbc1e4596b8f6ba59a9be0e829ed059f3932635
Jack <jackpronske@yahoo.com>
2020-12-24 10:54:41 -0800

    implemented correct code for reverseArr

--- src/recursion.js
+++ src/recursion.js
@@ -347,10 +347,26 @@ var compareStr = function(str1, str2) {
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+
+  var result = [];
+  if (str.length === 0) {
+    return result;
+  }
+  result.push(str.charAt(0));
+  return result.concat(createArray(str.slice(1)));
+
 };
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+
+  var result = [];
+  if (array.length === 0) {
+    return result;
+  }
+  result.push(array[array.length - 1]);
+  return result.concat(reverseArr(array.slice(0, array.length - 1)));
+
 };
 
 // 18. Create a new array with a given value and length.
