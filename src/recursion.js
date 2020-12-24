a04910e3a148369147f1a3828907b7e1b248ad7f
Jack <jackpronske@yahoo.com>
2020-12-24 14:54:54 -0800

    implemented correct solution for problem 23

--- src/recursion.js
+++ src/recursion.js
@@ -468,12 +468,31 @@ var countKeysInObj = function(obj, key) {
 
 };
 
+
+
 // 23. Write a function that counts the number of times a value occurs in an object.
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+
+  var result = 0;
+
+  for (var element in obj) {
+    if (obj[element] === value) {
+      result += 1;
+    }
+
+    if (typeof obj[element] === 'object') {
+      result += countValuesInObj(obj[element], value);
+    }
+  }
+
+  return result;
+
 };
+
+
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
