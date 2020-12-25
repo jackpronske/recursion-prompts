7d94cc58973767b23c7a52fe4a70cc1785826eef
Jack <jackpronske@yahoo.com>
2020-12-24 18:39:58 -0800

    implemented a correct code function for fibArray

--- src/recursion.js
+++ src/recursion.js
@@ -521,6 +521,19 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+
+  if (n <= 0) return null;
+  if (n < 2) {
+    return [0, 1];
+  }
+  if (n < 3) {
+    return [0, 1, 1];
+  }
+
+  var previousFibArr = fibonacci(n - 1);
+  previousFibArr.push(previousFibArr[n - 2] + previousFibArr[n - 1]);
+  return previousFibArr
+
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
