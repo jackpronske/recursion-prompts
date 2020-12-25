5e9a6448e4126bf4ae6d132bc16b470a9cf65fa3
Jack <jackpronske@yahoo.com>
2020-12-24 18:17:12 -0800

    implememted a correct answer for changing keys in an object

--- src/recursion.js
+++ src/recursion.js
@@ -497,6 +497,22 @@ var countValuesInObj = function(obj, value) {
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+
+  var objArr = Object.keys(obj);
+
+	objArr.forEach(element => {
+		if (typeof obj[element] === 'object') {
+			obj[element] = replaceKeysInObj(obj[element], oldKey, newKey);
+    }
+
+		if (element === oldKey) {
+			obj[newKey] = obj[element];
+			delete obj[element];
+		}
+  });
+
+  return obj
+
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
